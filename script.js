8077ce928e446e1c432abc65a3ac6c0db3df0645
ManuelGurbanov <manuelgurbanov@gmail.com>
2024-05-19 08:56:37 -0300

    added api fetch

---
+++ script.js
@@ -0,0 +1,30 @@
+// Reemplaza '[yourkey]' con tu API Key válida
+const apiKey = 'abf601b2';
+const apiUrl = `http://www.omdbapi.com/?apikey=${apiKey}&`;
+
+async function searchMovies() {
+    const searchInput = document.getElementById('searchInput').value;
+    const response = await fetch(`${apiUrl}s=${searchInput}`);
+    const data = await response.json();
+    displayMovies(data.Search);
+}
+
+function displayMovies(movies) {
+    const moviesGrid = document.getElementById('moviesGrid');
+    moviesGrid.innerHTML = '';
+
+    movies.forEach(movie => {
+        const movieElement = document.createElement('div');
+        movieElement.classList.add('movie');
+        movieElement.innerHTML = 
+        `
+            <a href="movie-details.html">
+                <img src="${movie.Poster}" alt="${movie.Title}" class="">
+            </a>
+            <div class="movieTittle">
+                <h5>${movie.Title}</h5>
+            </div>
+        `;
+        moviesGrid.appendChild(movieElement);
+    });
+}
